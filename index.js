0eae1246e51c6b5eec076f0c07fcb3b98091f1e7
Maximilien L <maximilien.lemoine.pro@gmail.com>
2022-10-23 18:02:29 +0200

    add express server + socket.io

---
+++ index.js
@@ -0,0 +1,42 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const favicon = require('serve-favicon')
+const path = require('path')
+const io = require('socket.io')(server, { cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+`/public/`)
+});
+
+app.get('/back', (req, res) => {
+    res.sendFile(__dirname+`/public/back/`)
+});
+
+
+io.on('connection', (socket) =>{ //Récupération des connexions des interfaces front et back
+    socket.on('connexion', (loca) => {
+        let datetime = new Date()
+        console.log(datetime.getHours() + ' : ' + datetime.getMinutes() + ' | Interface '+ loca + ' connecté.') //Connexion
+
+        socket.on("disconnect", (reason) =>{ //Déconnexion
+            console.log(datetime.getHours() + ' : ' + datetime.getMinutes() + ' | Interface '+ loca + ' déconnecté pour : ' + reason+'.')
+        })
+
+        socket.on('BackToServer', (info) => { //récupération des informaions par le serveur du back
+            console.log('Back send : ' + info);
+
+            socket.broadcast.emit('ServerToFront', info); //Envoie des informations au front par le serveur
+        });
+    })
+});
+
+server.listen(3000, ()=> {
+    console.log('Démarré.')
+})
